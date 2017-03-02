7db34e007f67bbd91702303fb70a7ab38a29487c
Jose Vila <you@example.comJDVila@gmail.com>
2017-03-02 14:44:38 -0500

    added auth listeners

--- app/src/main/java/com/rooksoto/parallel/fragments/activityLogin/FragmentLoginLogin.java
+++ app/src/main/java/com/rooksoto/parallel/fragments/activityLogin/FragmentLoginLogin.java
@@ -1,19 +1,26 @@
 package com.rooksoto.parallel.fragments.activityLogin;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.rooksoto.parallel.R;
 
 public class FragmentLoginLogin extends Fragment {
     private View mView;
     private String username;
     private String password;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private final String TAG = getContext().toString();
 
     @Nullable
     @Override
@@ -21,6 +28,41 @@ public class FragmentLoginLogin extends Fragment {
         mView = inflater.inflate(R.layout.fragment_login_login, container, false);
         initialize();
         return mView;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
     private void initialize () {
